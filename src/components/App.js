0e8e63718966f82a3601e7bfd288cb28d5ca3c17
Sean McPherson <seanmcp25@gmail.com>
2017-09-20 15:09:24 -0400

    Create Player component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import Player from './Player'
+
 class App extends Component {
   constructor() {
+    super()
     this.state = {
       results: []
     }
---
+++ src/components/Player.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <audio id="audioPlayer" src="">
+        Your browser does not support the <code>audio</code> element.
+      </audio>
+    );
+  }
+}
